a0cdc498a72254a810ac79b234402b77e3810e8c
Ruben Leija <ruben.leija@me.com>
2018-01-22 20:09:51 -0500

    Add filter and sort features.
    Sort list of students by courses and sort by name or grade (ascending or descending order)

--- index.js
+++ index.js
@@ -1,47 +1,87 @@
 const fetch = require('isomorphic-fetch')
 
-// TODO: From the answer function, return an array of objects
-// with the name and average Math grade of each student that
-// is getting less than an average of 70 in Math.
-// Round the average grade to the nearest whole number.
-// NOTE: Visit https://gist.github.com/petersondrew/4aa8ae630a283a7b942fb693047fc4d7 to see input data
+/**
+ * Find average test grade of course
+ * 
+ * @param {Array} tests List of test grades
+ */
+const findCourseAverage = (tests, rounded = true) => {
+  const gradeSum = tests.reduce((total, test) => test.grade + total, 0)
+  const averageGrade = gradeSum / tests.length
 
-// Get student's average math grade
-const getStudentMathAverageGrade = (student = null) => {
-  // Get math course & math average grade
-  const mathCourse = student.classes.find(course => course.name.toLowerCase() === 'math');
-  const mathAverageGrade = mathCourse.tests.reduce((total, test) => test.grade + total, 0) / mathCourse.tests.length;
-  return  Math.round(mathAverageGrade);
+  if (rounded) return Math.round(averageGrade)
+
+  return {floatAvg: averageGrade, intAvg: Math.round(averageGrade)}
 }
 
-const answer = (students) => {
-  let failingStudents = [];
 
-  // Grab students who are taking a math course.
-  const mathStudents = students.filter(student => student.classes.filter(c => c.name.toLowerCase() === 'math').length);
+/**
+ * Answer
+ * 
+ * Get students failing below a specific grade average on a specific course.
+ *
+ * @param {Array}      students List of students.
+ * @param {String}       course Course category.
+ * @param {String}         sort Sort type.
+ * @param {Number} gradeAverage Grade average.
+ */
+const answer = (students = [], courseCategory = 'math', sort = 'name', gradeAverage = 70) => {
+  let results = []
 
-  // Return empty array if students don't have a math course
-  if (!mathStudents.length) return failingStudents;
+  // Filter students by course
+  const studentList = students.filter(student => student.classes.filter(c => c.name.toLowerCase() === courseCategory).length)
 
-  // Get failing students below grade average of 70
-  failingStudents = mathStudents.filter(student => getStudentMathAverageGrade(student) < 70)
-    .map(student => Object.assign({}, student, {averageGrade: getStudentMathAverageGrade(student)}));
-  
-  return failingStudents;
-};
+  if (studentList.length) {
+    const failingStudents = studentList
+      .filter(students => {
+        const course = students.classes.find(course => course.name.toLowerCase() === courseCategory)
+        const courseGradeAverage = findCourseAverage(course.tests)
+        return courseGradeAverage < gradeAverage
+      })
 
+    results = failingStudents.map(student => {
+      const course = student.classes.find(course => course.name.toLowerCase() === courseCategory)
+      const courseGradeAverage = findCourseAverage(course.tests, false)
+      return Object.assign({}, student, {
+        courseResults: {
+          courseCategory,
+          floatAvg: courseGradeAverage.floatAvg,
+          intAvg: courseGradeAverage.intAvg
+        }})
+    });
+
+    if (sort === 'name') {
+      results = results.slice(0).sort((a, b) => {
+        const aName = a.name.trim()
+        const bName = b.name.trim()
+
+        if(aName < bName) return -1;
+        if(aName > bName) return 1;
+        return 0;
+      })
+    } else {
+      results = results.slice(0).sort((a, b) => a.courseResults.floatAvg - b.courseResults.floatAvg)
+
+      if (sort === 'desc') results.reverse()
+    }
+
+  }
+
+  return results
+}
+
+/**
+ * Fetch students from gist api endpoint and return promise with all students
+ */
 const fetchStudents = () => {
   return fetch("https://gist.githubusercontent.com/petersondrew/4aa8ae630a283a7b942fb693047fc4d7/raw/7c9d643400449e9cbd51faa3afe63c7c28b1dfa7/students.json")
-    .then(res => res.json());
-};
+    .then(res => res.json())
+}
 
-// Example output:
-// Nickolas Leinen - 65
-// Coralee Heilman - 68
 fetchStudents()
   .catch(err => console.error(`Error fetching students, check your internet connection. ${err}`))
   .then(students => {
-    answer(students).forEach(student => {
-      console.log(`${student.name} - ${student.averageGrade}`);
+    answer(students, 'reading', 'desc', 90).forEach(student => {
+      console.log(`${student.name} - ${student.courseResults.intAvg}`);
     });
   });
